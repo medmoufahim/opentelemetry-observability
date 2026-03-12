452e7dc2ed18df2c775d9b79f9037a11f9a1d3a0
medmoufahim <med.moufahim@gmail.com>
2026-03-12 15:36:45 +0000

    add distributed tracing

--- 01-trace-flix/actor-service/src/main/java/com/vinsguru/actor/controller/ActorController.java
+++ 01-trace-flix/actor-service/src/main/java/com/vinsguru/actor/controller/ActorController.java
@@ -2,13 +2,20 @@ package com.vinsguru.actor.controller;
 
 import com.vinsguru.actor.dto.ActorDto;
 import com.vinsguru.actor.service.ActorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ActorController {
+
+    private static final Logger log = LoggerFactory.getLogger(ActorController.class);
 
     private final ActorService actorService;
 
@@ -17,7 +24,8 @@ public class ActorController {
     }
 
     @GetMapping("/api/actors/{id}")
-    public ResponseEntity<ActorDto> getActor(@PathVariable Integer id) {
+    public ResponseEntity<ActorDto> getActor(@PathVariable Integer id, @RequestHeader Map<String, String> headers) {
+        log.info("Request Headers: {}", headers);
         return this.actorService.getActor(id)
                                 .map(ResponseEntity::ok)
                                 .orElseGet(() -> ResponseEntity.notFound().build());
